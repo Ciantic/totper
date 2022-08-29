644cce6b6b4f6e64ff2ea83bf12c3d60b307b6ce
Jari Pennanen <ciantic@oksidi.com>
2022-08-29 21:59:08 +0300

    pipe support

--- src/main.rs
+++ src/main.rs
@@ -5,41 +5,45 @@ use totp_lite::{totp_custom, Sha1, Sha256, Sha512};
 fn print_usage() {
     println!("Usage: totper SECRET <STEP> <DIGITS> <ALG> <TIMESTAMP>");
     println!("");
-    println!("  SECRET    = Unpadded base32 (RFC4648) encoded secret");
+    println!("       or pipe the input parameters");
+    println!("");
+    println!("  SECRET    = Unpadded Base32 (RFC4648) encoded secret");
     println!("  STEP      = Seconds of step, defaults to 30");
     println!("  DIGITS    = Number of digits in the result, defaults to 6");
-    println!("  ALG       = Algorithm sha1, sha256 or sha512, defaults to sha1");
+    println!("  ALG       = Algorithm SHA1, SHA256 or SHA512, defaults to SHA1");
     println!("  TIMESTAMP = UTC Unix timestamp in seconds, defaults to current");
     println!("");
-    println!("  Note: practically all services use sha1 as default");
+    println!("  Note: practically all services use SHA1 as default");
     println!("");
     println!("Example: ");
     println!("  totper KZXW6ZDPN4");
     println!("  766369");
     println!("");
+    println!("  echo KZXW6ZDPN4 | totper");
+    println!("  766369");
     println!("Repository: https://github.com/Ciantic/totp-cli");
 }
 
 fn try_do(args: Vec<String>) -> Result<String, String> {
     let secret = args
-        .get(1)
+        .get(0)
         .map(|f| decode(base32::Alphabet::RFC4648 { padding: false }, &f))
         .flatten()
         .ok_or("Secret was not given in right format")?;
 
     let step = args
-        .get(2)
+        .get(1)
         .map_or(Ok(30), |f| f.parse::<u64>())
         .map_err(|_| "Step must be integer")?;
 
     let digits = args
-        .get(3)
+        .get(2)
         .map_or(Ok(6), |f| f.parse())
         .map_err(|_| "Digits must be given as integer")?;
 
-    let alg: String = args.get(4).map_or("sha1".to_string(), |f| f.to_string());
+    let alg: String = args.get(3).map_or("sha1".to_string(), |f| f.to_string());
 
-    let seconds = args.get(5).map_or_else(
+    let seconds = args.get(4).map_or_else(
         || {
             SystemTime::now()
                 .duration_since(UNIX_EPOCH)
@@ -73,8 +77,18 @@ fn try_do(args: Vec<String>) -> Result<String, String> {
 }
 
 fn main() {
-    let args = std::env::args().collect::<Vec<_>>();
-    if args.len() == 1 {
+    let read_from_stdin = !atty::is(atty::Stream::Stdin);
+
+    // Read arguments from cli or stdin
+    let args: Vec<String> = if read_from_stdin {
+        let mut line = String::new();
+        std::io::stdin().read_line(&mut line).expect("Foo");
+        line.trim().split(" ").map(ToOwned::to_owned).collect()
+    } else {
+        std::env::args().skip(1).collect()
+    };
+
+    if args.len() == 0 {
         print_usage();
         return;
     }
